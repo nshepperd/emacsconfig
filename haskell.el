3b653d89c9d7d8b010c9917463dad6a23487b90d
Neil Shepperd <nshepperd@gmail.com>
2015-09-06 22:20:35 +1000

    Bind M-. to hayoo instead of tags, since I don't really use tags

--- haskell.el
+++ haskell.el
@@ -2,8 +2,16 @@
 
 (add-to-list 'exec-path "~/.local/bin")
 (custom-set-variables '(haskell-tags-on-save t))
+
 (add-hook 'haskell-mode-hook
-          (lambda () (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def-or-tag)))
+          (lambda ()
+            ;; (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def-or-tag)
+            (define-key haskell-mode-map (kbd "M-.")
+              (lambda ()
+                (interactive)
+                (browse-url (format "http://holumbus.fh-wedel.de/hayoo/hayoo.html?query=%s&start"
+                                    (thing-at-point 'word)))
+                ))))
 
 (autoload 'ghc-init "ghc" nil t)
 (autoload 'ghc-debug "ghc" nil t)
