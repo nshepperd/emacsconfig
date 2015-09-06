62caeab5a5a0d48a3e30676a6e4a84acc27e6e0e
Neil Shepperd <nshepperd@gmail.com>
2015-09-06 22:19:28 +1000

    switching to melpa-stable

--- packages.el
+++ packages.el
@@ -1,3 +1,3 @@
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")))
