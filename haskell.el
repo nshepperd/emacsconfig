08484d226668a9290c419579448a49e50b7fa44d
Neil Shepperd <nshepperd@gmail.com>
2015-09-06 22:21:07 +1000

    haskell: Disabling ghc-mod since it's broken right now

--- haskell.el
+++ haskell.el
@@ -13,6 +13,6 @@
                                     (thing-at-point 'word)))
                 ))))
 
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+;; (autoload 'ghc-init "ghc" nil t)
+;; (autoload 'ghc-debug "ghc" nil t)
+;; (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
